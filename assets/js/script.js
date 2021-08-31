2b10c1459b9faa587501d167d4dab9dcc7b31e9f
tniemeye19 <timothy.niemeyer19@gmail.com>
2021-08-30 20:07:13 -0400

    added capture form field values

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,9 +5,25 @@ var createTaskHandler = function(event) {
 
     event.preventDefault();
 
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    // Create List Item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
+
+    // Create div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+
+    // Give it a class name
+    taskInfoEl.className - "task-info";
+
+    // Add HTML content to div
+    taskInfoEl.innterHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    
+    listItemEl.appendChild(taskInfoEl);
+
+    // Add entire list item to list
     tasksToDoEl.appendChild(listItemEl);
 }
 
